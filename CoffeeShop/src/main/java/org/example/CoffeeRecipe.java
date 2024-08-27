a29bdd6e3f0b9de894e67b0b34723b172c886a01
ozberk.dirik <ozberkkdirikk@gmail.com>
2024-08-27 10:01:32 +0300

    CoffeeRecipe classı metotları yazıldı

--- CoffeeShop/src/main/java/org/example/CoffeeRecipe.java
+++ CoffeeShop/src/main/java/org/example/CoffeeRecipe.java
@@ -1,4 +1,26 @@
 package org.example;
 
+import java.util.Map;
+
 public class CoffeeRecipe {
+    private String name ;
+
+    private Map<String,Integer> ingredients;
+
+    public CoffeeRecipe(String name , Map<String, Integer> ingredients){
+        this.name=name;
+        this.ingredients=ingredients;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public void prepare(){
+        System.out.println(name + "seçtiniz." + name + "içerisinde ");
+        ingredients.forEach((ingredient , amount)->{
+            System.out.println(amount + "doz" + ingredient);
+        });
+        System.out.println("Afiyet Olsun");
+    }
 }
